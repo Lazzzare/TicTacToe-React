bb600eb8cee1d87a95f999824861cc0a4b5ba451
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-22 15:17:16 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Board from "./components/Board";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-DarkNavy w-full h-screen flex items-center justify-center flex-col">
+      <Board />
+    </div>
+  );
 };
 
 export default App;
