4e1d1cd88b04b69e870087a3fb40deab92569c3e
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-22 12:55:57 +0400

    configure colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        LightBlue: "#31C3BD",
+        LightBlueHover: "#65E9E4",
+        LightYellow: "#F2B137",
+        LightYellowHover: "#FFC860",
+        DarkNavy: "#1A2A33",
+        SemiDarkNavy: "#1F3641",
+        Silver: "#A8BFC9",
+        SilverHover: "#DBE8ED",
+      },
+    },
   },
   plugins: [],
-}
+};
