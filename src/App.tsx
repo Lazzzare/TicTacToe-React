f168b84556a210e9f264e1e12a3bb53d60d9d57a
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-26 14:26:26 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./components/HomePage";
+import Board from "./components/Board";
 
 const App = () => {
   return (
-    <div className="bg-DarkNavy w-full h-screen flex items-center justify-center flex-col"></div>
+    <div className="bg-DarkNavy w-full h-screen flex items-center justify-center flex-col">
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/board" element={<Board />}></Route>
+      </Routes>
+    </div>
   );
 };
 
