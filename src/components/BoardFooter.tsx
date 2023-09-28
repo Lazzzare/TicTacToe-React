232063eab46802bd3b4a896e8e8df0c77d32cc91
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-28 18:17:44 +0400

    Update BoardFooter.tsx

--- src/components/BoardFooter.tsx
+++ src/components/BoardFooter.tsx
@@ -1,4 +1,16 @@
-const BoardFooter = () => {
+interface Props {
+  winCountX: number;
+  winCountY: number;
+  winner: string | null;
+  tiesCount: number;
+}
+
+const BoardFooter = ({
+  winCountX,
+  winCountY,
+  // winner,
+  tiesCount,
+}: Props) => {
   return (
     <div className="flex flex-row justify-between w-full mt-5">
       <div className="pt-[13px] pb-[11px] px-10 bg-LightBlue rounded-2xl flex flex-col text-center">
@@ -6,7 +18,7 @@ const BoardFooter = () => {
           X (P2)
         </h4>
         <h2 className="text-2xl font-bold tracking-[1.5px] text-DarkNavy">
-          14
+          {winCountX}
         </h2>
       </div>
       <div className="pt-[13px] pb-[11px] px-10 bg-Silver rounded-2xl flex flex-col text-center">
@@ -14,14 +26,16 @@ const BoardFooter = () => {
           TIES
         </h4>
         <h2 className="text-2xl font-bold tracking-[1.5px] text-DarkNavy">
-          24
+          {tiesCount}
         </h2>
       </div>
       <div className="pt-[13px] pb-[11px] px-10 bg-LightYellow rounded-2xl flex flex-col text-center">
         <h4 className="text-sm tracking-[0.875px] font-medium text-DarkNavy">
           O (P1)
         </h4>
-        <h2 className="text-2xl font-bold tracking-[1.5px] text-DarkNavy">8</h2>
+        <h2 className="text-2xl font-bold tracking-[1.5px] text-DarkNavy">
+          {winCountY}
+        </h2>
       </div>
     </div>
   );
