d59d2555ee22c5f61d667b412bb9e97070215933
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-28 18:17:46 +0400

    Update Board.tsx

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,12 +5,18 @@ import Square from "./Square";
 import BoardHeader from "./BoardHeader";
 import WinnerBox from "./WinnerBox";
 import BoardFooter from "./BoardFooter";
+import RefreshBox from "./RefreshBox";
+import TieBox from "./TieBox";
 
 const Board = () => {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(""));
-
-  console.log(squares);
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [squares, setSquares] = useState<string[]>(Array(9).fill(""));
+  const [refresh, setRefresh] = useState<boolean>(false);
+  const [winCountX, setWinCountX] = useState<number>(0);
+  const [winners, setWinner] = useState<string | null>(null);
+  const [tiesCount, setTiesCount] = useState<number>(0);
+  const [winCountY, setWinCountY] = useState<number>(0);
+  const [isTie, setIsTie] = useState<boolean>(false);
 
   const calculateWinner = (squares: string[]) => {
     const lines = [
@@ -38,20 +44,29 @@ const Board = () => {
     return null;
   };
 
-  console.log("calculate", calculateWinner(squares));
-
   function handleClick(i: number) {
-    if (calculateWinner(squares) || squares[i]) {
+    const newSquares = squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
       return;
     }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
-    }
-    setSquares(nextSquares);
+
+    newSquares[i] = xIsNext ? "X" : "O";
+    setSquares(newSquares);
     setXIsNext(!xIsNext);
+
+    if (!newSquares.includes("") && !calculateWinner(newSquares)) {
+      setTiesCount((ties) => ties + 1);
+      setIsTie(true);
+    }
+
+    const winner = calculateWinner(newSquares);
+    if (winner) {
+      if (winner === "X") {
+        setWinCountX((prevCount) => prevCount + 1);
+      } else if (winner === "O") {
+        setWinCountY((prevCount) => prevCount + 1);
+      }
+    }
   }
 
   const winner = calculateWinner(squares);
@@ -62,32 +77,55 @@ const Board = () => {
     status = (xIsNext ? "X" : "O") + " TURN";
   }
 
-  console.log("Winner", winner);
+  const handleNewRound = () => {
+    setSquares(Array(9).fill(""));
+    setXIsNext(true);
+    setWinner(null);
+  };
 
   return (
     <>
-      {/* BoardHeader */}
-      <BoardHeader status={`${status}`} winner={winner} />
-      {/* Board */}
-      <div className="flex flex-col">
-        <div className="flex gap-x-5 mb-5">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <div className={`${winner || refresh ? "opacity-30" : "opacity-100"}`}>
+        {/* BoardHeader */}
+        <BoardHeader
+          status={`${status}`}
+          winner={winner}
+          setRefresh={setRefresh}
+        />
+        {/* Board */}
+        <div className="flex flex-col">
+          <div className="flex gap-x-5 mb-5">
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          </div>
+          <div className="flex gap-x-5 mb-5">
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          </div>
+          <div className="flex gap-x-5">
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          </div>
         </div>
-        <div className="flex gap-x-5 mb-5">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="flex gap-x-5">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
-        <BoardFooter />
+        <BoardFooter
+          winCountX={winCountX}
+          winCountY={winCountY}
+          winner={winner}
+          tiesCount={tiesCount}
+        />
       </div>
-      {winner ? <WinnerBox winner={winner} /> : null}
+      {winner ? (
+        <WinnerBox winner={winner} onNewRound={handleNewRound} />
+      ) : null}
+      {refresh ? (
+        <RefreshBox refresh={refresh} setRefresh={setRefresh} />
+      ) : null}
+      {isTie ? (
+        <TieBox onNewRound={handleNewRound} isTie={isTie} setIsTie={setIsTie} />
+      ) : null}
     </>
   );
 };
