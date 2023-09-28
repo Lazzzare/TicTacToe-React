badf9101c6dc4f6f85886d1f283747932b4cb938
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-28 19:17:36 +0400

    Update BoardFooter.tsx

--- src/components/BoardFooter.tsx
+++ src/components/BoardFooter.tsx
@@ -13,7 +13,7 @@ const BoardFooter = ({
 }: Props) => {
   return (
     <div className="flex flex-row justify-between w-full mt-5">
-      <div className="pt-[13px] pb-[11px] px-10 bg-LightBlue rounded-2xl flex flex-col text-center">
+      <div className="py-3 px-5 md:pt-[13px] md:pb-[11px] md:px-10 w-[96px] md:w-[140px] bg-LightBlue rounded-2xl flex flex-col text-center">
         <h4 className="text-sm tracking-[0.875px] font-medium text-DarkNavy">
           X (P2)
         </h4>
@@ -21,7 +21,7 @@ const BoardFooter = ({
           {winCountX}
         </h2>
       </div>
-      <div className="pt-[13px] pb-[11px] px-10 bg-Silver rounded-2xl flex flex-col text-center">
+      <div className="py-3 px-5 md:pt-[13px] md:pb-[11px] md:px-10 w-[96px] md:w-[140px] bg-Silver rounded-2xl flex flex-col text-center">
         <h4 className="text-sm tracking-[0.875px] font-medium text-DarkNavy">
           TIES
         </h4>
@@ -29,7 +29,7 @@ const BoardFooter = ({
           {tiesCount}
         </h2>
       </div>
-      <div className="pt-[13px] pb-[11px] px-10 bg-LightYellow rounded-2xl flex flex-col text-center">
+      <div className="py-3 px-5 md:pt-[13px] md:pb-[11px] md:px-10 w-[96px] md:w-[140px] bg-LightYellow rounded-2xl flex flex-col text-center">
         <h4 className="text-sm tracking-[0.875px] font-medium text-DarkNavy">
           O (P1)
         </h4>
